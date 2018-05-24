f439e9dde9617a7463fe1a16bfc69d53fa08bf41
christopher Thronebury <Chris@christophers-MacBook.local>
2018-05-23 22:51:57 -0400

    implement add new genre route

--- index.js
+++ index.js
@@ -18,6 +18,29 @@ app.get("/api/genres", (req, res) => {
   res.send(genres);
 });
 
+// Add New Genre Route
+app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+
+  genres.push(genre);
+  res.send(genre);
+});
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(genre, schema);
+}
+
 // Set port from env variable or default ot 3000
 const port = process.env.PORT || 4000;
 
