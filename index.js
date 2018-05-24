7aa7c4c52dd6d263217e0ae6d55590cf97d56f2a
christopher Thronebury <Chris@christophers-MacBook.local>
2018-05-23 22:48:38 -0400

    Implement get genres route

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const app = express();
 
 app.use(express.json());
 
+// Create Genre Array for Dummy Data
+
+const genres = [
+  { id: 1, name: "Horror" },
+  { id: 2, name: "Action" },
+  { id: 3, name: "Comedy" }
+];
+
+// View All Genres Route
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
 // Set port from env variable or default ot 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Set up app listener
 app.listen(port, console.log(`Listening on port ${port}`));
