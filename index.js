102b5139ca615b0ccb103118f048409fbf9847e4
christopher Thronebury <Chris@christophers-MacBook.local>
2018-05-23 22:55:24 -0400

    Implement update route

--- index.js
+++ index.js
@@ -32,6 +32,21 @@ app.post("/api/genres", (req, res) => {
   res.send(genre);
 });
 
+// Update a genre
+app.put("/api/genres/:id", (req, res) => {
+  // Look up genre and return 404 if it doesn't exist
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("That genre was not found");
+
+  // Validate genre and if it's invalid, return 400 - Bad Request
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Update the genre
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
 function validateGenre(genre) {
   const schema = {
     name: Joi.string()
