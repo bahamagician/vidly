fc1ec5d488f729b9d3dfafe91baa07a311fc88be
SC Graphics <scgraphics@SCs-iMac.local>
2018-05-28 16:24:39 -0400

    implement registration route

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
 
 // Connect to mongo
 mongoose
@@ -21,6 +22,7 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 // Set port from env variable or default ot 3000
 const port = process.env.PORT || 4000;
---
+++ models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+// Define user schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "You must enter a name.",
+    minlength: 5,
+    maxlength: 50,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: "You must enter an email.",
+    minlength: 5,
+    maxlength: 255,
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: "You must enter a password",
+    trim: true,
+    minlength: 5,
+    maxlength: 1024
+  }
+});
+
+// Define Customer Model
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+  };
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
+exports.userSchema = userSchema;
